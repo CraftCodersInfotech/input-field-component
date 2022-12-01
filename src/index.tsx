ddbf1ef68e266da4daed64ebfe6c8be6b10bede8
Hitesh Kachariya <hiteshk086@gmail.com>
2022-12-01 14:24:57 +0530

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,9 @@ import {
 } from "react-native";
 import * as React from "react";
 import { useState } from "react";
+import * as yup from "yup";
+import { Control, Controller, FieldValues, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface props {
   placeholder?: string;
@@ -22,10 +25,13 @@ interface props {
   showIcon?: boolean;
   containerStyle: StyleProp<ViewStyle>;
   inputStyle: StyleProp<ViewStyle>;
+  control: Control<FieldValues, any>;
+  errors: any;
 }
 
 const InputField = (props: props) => {
   const [visible, setVisible] = useState(false);
+
   const {
     placeholder, //The string that will be rendered before text input has been entered.
     value, //The value to show for the text input.
@@ -35,6 +41,8 @@ const InputField = (props: props) => {
     showIcon, //When to show image by side textInput
     containerStyle, //Give style to main view
     inputStyle, // Give style to TextInput
+    control,
+    errors,
   } = props;
 
   return (
@@ -43,18 +51,29 @@ const InputField = (props: props) => {
         { ...containerStyle, flexDirection: "row", paddingHorizontal: 5 },
       ]}
     >
-      <TextInput
-        secureTextEntry={visible}
-        onChangeText={onChangeText}
-        returnKeyType={"done"}
-        style={inputStyle} //use it when use icon
-        value={value}
-        keyboardType={keyboardType}
-        maxLength={maxLength}
-        placeholder={placeholder}
-        placeholderTextColor={"gray"}
-        numberOfLines={1}
-      />
+      <Controller
+        name="name"
+        control={control}
+        render={({ field: { onBlur, onChange, value } }) => (
+          <TextInput
+            secureTextEntry={visible}
+            onChangeText={onChange}
+            returnKeyType={"done"}
+            style={inputStyle} //use it when use icon
+            value={value}
+            keyboardType={keyboardType}
+            maxLength={maxLength}
+            placeholder={placeholder}
+            placeholderTextColor={"gray"}
+            numberOfLines={1}
+            onBlur={onBlur}
+          />
+        )}
+      ></Controller>
+      {errors && <Text style={inputStyle.errorMsg}>{errors.message}</Text>}
+      {/* {errors.name && (
+        <Text style={inputStyle.errorMsg}>{errors.name.message}</Text>
+      )} */}
       {showIcon && (
         <TouchableOpacity onPress={() => setVisible(!visible)}>
           <View>
