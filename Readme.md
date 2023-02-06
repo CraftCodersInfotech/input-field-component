c4e1dbcc37ccdc22da84e1fffebbcdeb161d1e63
iMac <imac@192.168.1.27>
2023-02-06 13:03:37 +0530

    initla commit

--- Readme.md
+++ Readme.md
@@ -20,7 +20,7 @@
 To add the react-native-input-field to React Native app, run this command with tag version
 
 ```
-npm install react-native-input-field
+npm install https://github.com/CraftCodersInfotech/input-field-component.git
 ```
 
 Make sure to install pod into react-native Project after linking given command
@@ -29,24 +29,38 @@ Make sure to install pod into react-native Project after linking given command
 
 ### InputField
 
-| Name         | Type              | Description       |
-| :----------- | :---------------- | :---------------- |
-| placeholder  | string (Require)  | Formik field name |
-| height       | number (Optional) | Default is `0`    |
-| width        | string (Optional) | Default is `0`    |
-| borderWidth  | number (Optional) | Default is `1`    |
-| borderRadius | number (Optional) | Default is `0`    |
+| Name            | Type         | Description                                                                                               |
+| :-------------- | :----------- | :-------------------------------------------------------------------------------------------------------- |
+| placeholder     | string       | The string that will be rendered before text input has been entered.                                      |
+| secureTextEntry | boolean      | secureTextEntry                                                                                           |
+| onChangeText    | void         | Called when the text input's text changes.                                                                |
+| value           | string       | The value to show for the text input.                                                                     |
+| keyboardType    | KeyboardType | Determines which keyboard to open, e.g.numeric,default,number-pad,decimal-pad,email-address,phone-pad,url |
+| maxLength       | number       | maximum number of characters that can be entered.                                                         |
+| showIcon        | boolean      | When to show image by side textInput                                                                      |
+| containerStyle  | ViewStyle    | Give style to main view                                                                                   |
+| inputStyle      | ViewStyle    | Give style to TextInput                                                                                   |
 
 - Example
 
 ```javascript
-import TextInput from "react-native-input-field";
+import TextInput from 'react-native-input-field';
 
 const App = () => {
   return (
-    <View>
-      <TextInput placeholder={"Formik field name"} />
-    </View>
+    <TextInput
+      placeholder={'User name'}
+      containerStyle={{
+        height: 40,
+        width: '80%',
+        borderWidth: 2,
+        alignItems: 'center',
+      }}
+      inputStyle={{
+        width: '90%',
+      }}
+      showIcon // Use only when to render image
+    />
   );
 };
 ```
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,96 @@
-import { StyleSheet, Text, View, TextInput } from "react-native";
-import React from "react";
-export type props = {
-  height?: number;
-  width?: string;
-  borderWidth?: number;
-  borderRadius?: number;
-  placeholder: string;
-};
-
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  ViewStyle,
+  StyleProp,
+  KeyboardType,
+  Text,
+} from "react-native";
+import * as React from "react";
+import { useState } from "react";
+import { Control, Controller, FieldValues } from "react-hook-form";
+interface props {
+  placeholder?: string;
+  value?: string;
+  keyboardType?: KeyboardType;
+  maxLength?: number;
+  onChangeText?: () => void;
+  secureTextEntry?: boolean;
+  onPress?: () => void;
+  showIcon?: boolean;
+  containerStyle: StyleProp<ViewStyle>;
+  inputStyle: StyleProp<ViewStyle>;
+  control: Control<FieldValues, any>;
+  errors: any;
+  name: string;
+}
 const InputField = (props: props) => {
-  const { height, width, borderWidth, borderRadius, placeholder } = props;
+  const [visible, setVisible] = useState(false);
+  const {
+    placeholder, //The string that will be rendered before text input has been entered.
+    value, //The value to show for the text input.
+    keyboardType, //Determines which keyboard to open, e.g.numeric,default,number-pad,decimal-pad,email-address,phone-pad,url
+    maxLength, // maximum number of characters that can be entered.
+    onChangeText, //Called when the text input's text changes.
+    showIcon, //When to show image by side textInput
+    containerStyle, //Give style to main view
+    inputStyle, // Give style to TextInput
+    control,
+    errors,
+    name,
+  } = props;
   return (
-    <View
-      style={{
-        borderRadius: borderRadius ? borderRadius : 0,
-        borderWidth: borderWidth ? borderWidth : 1,
-        height: height ? height : 0,
-        width: width ? width : 0,
-        padding: 10,
-      }}
-    >
-      <TextInput placeholder={placeholder} />
-    </View>
+    <>
+      <View
+        style={[containerStyle, { flexDirection: "row", paddingHorizontal: 5 }]}
+      >
+        <Controller
+          name={name}
+          control={control}
+          render={({ field: { onBlur, onChange, value } }) => (
+            <TextInput
+              secureTextEntry={visible}
+              onChangeText={onChange}
+              returnKeyType={"done"}
+              style={inputStyle} //use it when use icon
+              value={value}
+              keyboardType={keyboardType}
+              maxLength={maxLength}
+              placeholder={placeholder}
+              placeholderTextColor={"gray"}
+              numberOfLines={1}
+              onBlur={onBlur}
+            />
+          )}
+        ></Controller>
+        {showIcon && (
+          <TouchableOpacity onPress={() => setVisible(!visible)}>
+            <View>
+              {visible ? (
+                <Image
+                  style={style.eyeClose}
+                  source={require("../src/assets/eyeClose.png")}
+                />
+              ) : (
+                <Image
+                  style={style.eyeOpen}
+                  source={require("../src/assets/eyeOpen.png")}
+                />
+              )}
+            </View>
+          </TouchableOpacity>
+        )}
+      </View>
+      {errors && <Text style={style.errorMsg}>{errors[name]?.message}</Text>}
+    </>
   );
 };
-
 export default InputField;
-
-const styles = StyleSheet.create({});
+const style = StyleSheet.create({
+  eyeClose: { height: 30, width: 30 },
+  eyeOpen: { height: 30, width: 30 },
+  errorMsg: { color: "red" },
+});
